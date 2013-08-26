47f437b4d8cff54b489924db99f782a7f7bd851d
Ian <typppo@gmail.com>
2013-08-25 20:35:27 -0700

    log scale

--- test.py
+++ test.py
@@ -5,9 +5,6 @@ import os
 import f2n
 import pyfits
 
-#hdulist = pyfits.open('fits.fits')
-#hdulist = pyfits.open('01_12DEC08_FL0802_0001.fits')
-
 def decompress(filepath):
   os.system('hdecompress %s' % filepath)
   return filepath.replace('arch.H', 'arch')
@@ -16,7 +13,7 @@ def process(compressed_filepath):
   filepath = decompress(compressed_filepath)
 
   im = f2n.fromfits(filepath)
-  im.makepilimage('lin', negative=False)
+  im.makepilimage('log', negative=False)
   im.tonet('nettest.png')
 
 if __name__ == "__main__":
